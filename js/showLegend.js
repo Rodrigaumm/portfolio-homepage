a4998677d0dffa46cc9d2b4618a7e676abed14be
Rodrigaumm <rodrigodejesus2208@gmail.com>
2023-09-17 10:06:59 -0300

    Remove read-more, skill-level-legend sections and its associations

--- js/showLegend.js
+++
@@ -1,7 +0,0 @@
-const legend = document.querySelector('.skill-lvl-legend')
-const showBtn = document.getElementById('readMore');
-
-showBtn.onclick = event=>{
-    event.preventDefault();
-    legend.classList.toggle('visible');
-}
